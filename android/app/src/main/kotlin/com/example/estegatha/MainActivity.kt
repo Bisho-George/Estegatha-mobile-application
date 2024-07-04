63b9130035bbeae34178edcbb191b047c20b5bb2
ahmedmostafa8701 <ahmedmostafa8701@gmail.com>
2024-07-05 01:57:12 +0300

    need fix first

--- android/app/src/main/kotlin/com/example/estegatha/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/estegatha/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.estegatha
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity : FlutterFragmentActivity() {
 
-class MainActivity: FlutterActivity()
+
+}
