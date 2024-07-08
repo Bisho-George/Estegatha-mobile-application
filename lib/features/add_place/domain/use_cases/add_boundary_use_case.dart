de7c8eddb1c0340ff956068b4a19a5f67ebc1257
bisho515 <bishogeorge689@gmail.com>
2024-07-08 16:35:18 +0300

    fix

--- lib/features/add_place/domain/use_cases/add_boundary_use_case.dart
+++ lib/features/add_place/domain/use_cases/add_boundary_use_case.dart
@@ -1,19 +1,20 @@
 import 'package:dartz/dartz.dart';
 import 'package:estegatha/features/add_place/domain/entities/organization_boundary_entity.dart';
-import 'package:estegatha/features/sign-up/data/failure/failure.dart';
-import 'package:estegatha/utils/helpers/use_case.dart';
 
+import '../../../../utils/helpers/failure.dart';
+import '../../../../utils/helpers/use_case.dart';
 import '../../data/models/organization_boundary_response.dart';
 import '../repos/boundary_repo.dart';
 
-class AddBoundaryUseCase extends UseCase<OrganizationBoundaryResponse, OrganizationBoundaryEntity> {
+class AddBoundaryUseCase
+    extends UseCase<OrganizationBoundaryResponse, OrganizationBoundaryEntity> {
   final BoundaryRepo _boundaryRepo;
 
   AddBoundaryUseCase(this._boundaryRepo);
 
   @override
-  Future<Either<Failure, OrganizationBoundaryResponse>> call(OrganizationBoundaryEntity param) async {
-     return await _boundaryRepo.addPlace(param);
+  Future<Either<Failure, OrganizationBoundaryResponse>> call(
+      OrganizationBoundaryEntity param) async {
+    return await _boundaryRepo.addPlace(param);
   }
-
 }
--- lib/utils/helpers/use_case.dart
+++ lib/utils/helpers/use_case.dart
@@ -3,5 +3,5 @@ import 'package:dartz/dartz.dart';
 import 'failure.dart';
 
 abstract class UseCase<Type, Param> {
-  Future<Either<Failure, Type>> call([Param param]);
+  Future<Either<Failure, Type>> call(Param param);
 }
